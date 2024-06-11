60038d929ae229929d3ae171e8cdc81b8a0655c5
prabhu0987 <prabhu.pallala@gmail.com>
2024-06-11 20:21:38 +0530

    create public subnet

--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "ibm-vpc" {
 resource "aws_subnet" "ibm-web-sn" {
   vpc_id     = aws_vpc.ibm-vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-north-1"
+  availability_zone = "eu-north-1a"
   map_public_ip_on_launch = "true"
 
   tags = {
