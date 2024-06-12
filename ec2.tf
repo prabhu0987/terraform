3ddf2a50c6c6dbeac1aa13be33bd1e08af82794d
prabhu0987 <prabhu.pallala@gmail.com>
2024-06-12 19:42:16 +0530

    web server

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ibm-web-server" {
   instance_type = "t3.micro"
   subnet_id = aws_subnet.ibm-web-sn.id
   key_name = "stock-key"
-  vpc_security_group_ids = aws_security_group.ibm-web-sg.id
+  vpc_security_group_ids = [aws_security_group.ibm-web-sg.id]
  
   tags = {
     Name = "ibm-web-server"
