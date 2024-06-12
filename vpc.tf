23fccc784ab2ebf990e2b58b06e4e952f2af82cf
prabhu0987 <prabhu.pallala@gmail.com>
2024-06-12 19:37:41 +0530

    ibm-ec2

--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "ibm-vpc" {
 
 #public subnet
 
-resource "aws_subnet" "-webibm-sn" {
+resource "aws_subnet" "ibm-web-sn" {
   vpc_id     = aws_vpc.ibm-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "eu-north-1a"
